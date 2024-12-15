082879f506f3e6e01fc1a51b3401f2aaad418def
Dav <david.sanchez.martinez@outlook.com>
2024-12-15 21:13:31 +0000

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <img src="public/chef-enzo.svg" alt="Chef Enzo" />
+      <h1><strong>Chef Enzo</strong></h1>
+    </header>
+  )
+}
+
+export default Header
