182fb16f0605eba88aac3396484debf52f2b61ec
Dav <david.sanchez.martinez@outlook.com>
2024-12-28 19:19:11 +0000

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,14 +8,20 @@ const Main = () => {
     <li key={ingredient}>{ingredient}</li>
   ))
 
+  function addIngredient(formData) {
+    const newIngredient = formData.get("ingredient")
+    setIngredients(prevIngredient => [...prevIngredient, newIngredient])
+  }
+
 
   return (
     <main>
-      <form className='form'>
+      <form action={addIngredient} className='form'>
         <input 
           type="text" 
           placeholder="e.g. avocado"
           aria-label="Add ingredient" 
+          name="ingredient"
         />
         <button>Add ingredient</button>
       </form>
