5ba09a0ec9ef3f9a5c94e930a7ab839a26f3d6ee
Dav <david.sanchez.martinez@outlook.com>
2024-12-30 19:58:12 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Header from './components/Header'
-import Main from './components/Main'
+import Home from './components/Home'
 import './App.css'
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <Header />
-      <Main />
+      <Home />
     </>
   )
 }
