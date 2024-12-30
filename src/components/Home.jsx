d6f11e6b736973a7eaec2c43ffd23a4b897d3dac
Dav <david.sanchez.martinez@outlook.com>
2024-12-30 21:30:10 +0000

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import EnzoRecipe from './EnzoRecipe'
+import IngredientList from './IngredientList'
 
 const Home = () => {
 
@@ -7,13 +8,11 @@ const Home = () => {
 
   const [recipeShown, setRecipeShown] = React.useState(false)
 
+
   function toggleRecipeShown() {
     setRecipeShown(prevShown => !prevShown)
   }
-
-  const ingredientsListItems = ingredients.map(ingredient => (
-    <li key={ingredient}>{ingredient}</li>
-  ))
+  
 
   function addIngredient(formData) {
     const newIngredient = formData.get("ingredient")
@@ -33,17 +32,7 @@ const Home = () => {
         <button>Add ingredient</button>
       </form>
 
-      {ingredients.length > 0 && <section>
-        <h2>Ingredients on hand:</h2>
-        <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
-        {ingredients.length > 3 && <div className="get-recipe-container">
-          <div>
-            <h3>Ready for a recipe?</h3>
-            <p>Generate a recipe from your list of ingredients.</p>
-          </div>
-          <button onClick={toggleRecipeShown}>Get a recipe</button>
-        </div>}
-      </section>}
+      {ingredients.length > 0 && <IngredientList ingredients={ ingredients } toggleRecipeShown={ toggleRecipeShown } />}
 
       {recipeShown && <EnzoRecipe />}
       
