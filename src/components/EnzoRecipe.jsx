cec3775bc46e735a0a56fe7b7733ede49aef17f7
Dav <david.sanchez.martinez@outlook.com>
2025-01-04 19:35:14 +0000

    Update EnzoRecipe.jsx

--- src/components/EnzoRecipe.jsx
+++ src/components/EnzoRecipe.jsx
@@ -4,7 +4,8 @@ import ReactMarkdown from 'react-markdown'
 const EnzoRecipe = ({recipe}) => {
 
     return (
-        <section>
+        <section className="suggested-recipe-container" aria-live="polite">
+            <h2>Chef Enzos's Recommendation:</h2>
             <ReactMarkdown>{recipe}</ReactMarkdown>
         </section>
     )
